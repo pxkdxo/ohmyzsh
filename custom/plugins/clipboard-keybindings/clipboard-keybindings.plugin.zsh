a91df172107321119577efcb8f40c5a78a54c064
Patrick DeYoreo <pdeyoreo@gmail.com>
2026-02-16 07:36:04 -0800

    Fixed typo

--- custom/plugins/clipboard-keybindings/clipboard-keybindings.plugin.zsh
+++ custom/plugins/clipboard-keybindings/clipboard-keybindings.plugin.zsh
@@ -1,9 +1,16 @@
 # clipboard-keybindings.plugin.zsh: define and bind copy-paste ZLE widgets
 
+# Copy the line editor buffer contents
+#
 _zclipcopy() {
-  printf '%s' -- "${BUFFER}" | clipcopy
+  printf '%s' "${BUFFER}" | clipcopy
 }
+zle -N _zclipcopy
+bindkey -M emacs '^[-' _zclipcopy
 
+
+# Paste the clipboard contents
+#
 _zclippaste() {
   if LBUFFER="${LBUFFER}$(clippaste)"; then
     zle redisplay
@@ -13,8 +20,14 @@ _zclippaste() {
   fi
 }
 
-zle -N _zclipcopy
-bindkey -M emacs '^[-' _zclipcopy
-
 zle -N _zclippaste
 bindkey -M emacs '^[+' _zclippaste
+
+
+# Quote the clipboard contents in-place
+#
+_zclipquote() {
+  printf '%q' "$(clippaste)" | clipcopy
+}
+zle -N _zclipquote
+bindkey -M emacs "^['" _zclipquote
